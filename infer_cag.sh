950b18ec309225e2f948f79538e2c003fd332aa8
kchyun <kchyun0910@gmail.com>
2025-05-28 01:54:13 +0000

    fix: minor

---
+++ infer_cag.sh
@@ -0,0 +1,4 @@
+CUDA_VISIBLE_DEVICES=1 python attention_statistics_cag.py \
+    --output_dir ./results/cag \
+    --txt_path dataset/txt_prompts/cag_prompts.txt \
+    --pag_scale 0.4 \
--- infer_pag.sh
+++
@@ -1,4 +0,0 @@
-CUDA_VISIBLE_DEVICES=1 python attention_statistics_pag.py \
-    --output_dir ./results/pag \
-    --txt_path dataset/txt_prompts/pag_prompts.txt \
-    --pag_scale 0.4 \
